d815d3d4fe9e54f6d3c901de302a16b70058ddaf
seunghwan <seosh817@naver.com>
2022-10-09 19:05:00 +0900

    Add CircularSeekBar View

---
+++ circularseekbar/src/main/java/com/seosh817/circularseekbar/CircularSeekBar.kt
@@ -0,0 +1,14 @@
+package com.seosh817.circularseekbar
+
+import android.content.Context
+import android.content.res.TypedArray
+import android.util.AttributeSet
+import android.view.View
+
+class CircularSeekBar @JvmOverloads constructor(
+    context: Context,
+    attributeSet: AttributeSet? = null,
+    defStyle: Int = 0
+) : FrameLayout(context, attributeSet, defStyle) {
+    
+}
