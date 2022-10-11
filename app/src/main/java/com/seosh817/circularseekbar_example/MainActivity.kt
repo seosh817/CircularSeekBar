90447a8b72627c8dbfd7e319009ead39c9c0b9e4
seunghwan <seosh817@naver.com>
2022-10-11 20:31:37 +0900

    Add Example app view

--- app/src/main/java/com/seosh817/circularseekbar_example/MainActivity.kt
+++ app/src/main/java/com/seosh817/circularseekbar_example/MainActivity.kt
@@ -1,11 +1,74 @@
 package com.seosh817.circularseekbar_example
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import androidx.appcompat.app.AppCompatActivity
+import com.seosh817.circularseekbar.BarStrokeCap
+import com.seosh817.circularseekbar_example.databinding.ActivityMainBinding
+import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            sbProgress.progress = circularSeekBar.progress.roundToInt()
+            sbStartAngle.progress = circularSeekBar.startAngle.roundToInt()
+            sbSweepAngle.progress = circularSeekBar.sweepAngle.roundToInt()
+            sbBarWidth.progress = circularSeekBar.barWidth.roundToInt()
+
+            sbProgress.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                    circularSeekBar.progress = progress.toFloat()
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+            })
+
+            sbStartAngle.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                    circularSeekBar.startAngle = progress.toFloat()
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+            })
+
+            sbSweepAngle.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                    circularSeekBar.sweepAngle = progress.toFloat()
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+            })
+
+            sbBarWidth.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                    circularSeekBar.barWidth = progress.toFloat()
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+            })
+
+            switchRounded.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) {
+                    circularSeekBar.barStrokeCap = BarStrokeCap.ROUND
+                } else {
+                    circularSeekBar.barStrokeCap = BarStrokeCap.SQUARE
+                }
+            }
+
+            switchInteractive.setOnCheckedChangeListener { _, isChecked ->
+                circularSeekBar.interactive = isChecked
+            }
+        }
     }
 }
