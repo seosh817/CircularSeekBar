0afca6a921c4fc5a66b2ce60b107df09f940f413
seunghwan <seosh817@naver.com>
2023-01-04 21:16:13 +0900

    Add dp to px to ViewExtensions

--- app/src/main/java/com/seosh817/circularseekbar_example/ViewExtension.kt
+++ app/src/main/java/com/seosh817/circularseekbar_example/ViewExtension.kt
@@ -1,6 +1,13 @@
 package com.seosh817.circularseekbar_example
 
 import android.content.res.Resources
+import android.util.DisplayMetrics
+import android.view.View
 
+/** px size to dp size. */
 val Float.dp: Float
+    get() = this / (Resources.getSystem().displayMetrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT)
+
+/** dp size to px size. */
+val Float.px: Float
     get() = this * Resources.getSystem().displayMetrics.density
